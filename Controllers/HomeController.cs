b4a0ac2bff3d1aa5557dd5a07c957b32eb3dde39
Andrzej Witkowski <endrjus8@gmail.com>
2020-05-09 12:58:02 +0200

    Razor basic functions.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product[] array =
+            {
+                new Product {Name = "Kajak", Price = 275M},
+                new Product {Name = "Kamizelka ratunkowa", Price = 48.95M},
+                new Product {Name = "Piłka nożna", Price = 19.50M},
+                new Product {Name = "Flaga narożna", Price = 34.95M}
+            };
+            return View(array);
+        }
+    }
+}
